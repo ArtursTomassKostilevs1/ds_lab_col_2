77f6156c1a4193b3d2d51ee1c17cc5cc552cc4a1
ArtursTomassKostilevs1 <144839324+ArtursTomassKostilevs1@users.noreply.github.com>
2024-04-21 12:20:25 +0000

    21042024

--- heap.py
+++ heap.py
@@ -57,11 +57,12 @@ class MaxHeap:
 
 
 def find_kth_smallest(nums, k):
-    ###################################
-    ###                             ###
-    ###                             ###
-    ###                             ###
-    ###################################
+    max_heap=MaxHeap()
+    for i in nums:
+        max_heap.insert(i)
+        if len(max_heap.heap) > k:
+            max_heap.remove()
+    return max_heap.heap[0]
 
 
 
